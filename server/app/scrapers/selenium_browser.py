46a71a48dc9409020911a4f532f2ff45e1a4faa0
Shanna Noe <snoe.prsvr@gmail.com>
2025-08-11 05:06:30 -0400

    Created selenium for scrapers

---
+++ server/app/scrapers/selenium_browser.py
@@ -0,0 +1,40 @@
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from app.utils.common import configure_driver
+import os
+
+def get_headless_browser():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")  
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=1920x1080")
+
+  
+    driver_path = "C:/Users/snoep_a5dedf8/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe"
+
+    if not os.path.isfile(driver_path):
+        raise FileNotFoundError(f"❌ ChromeDriver not found at: {driver_path}")
+
+    service = Service(driver_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    return driver
+def configure_webdriver():
+    options = uc.ChromeOptions()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+    driver = uc.Chrome(options=options, headless=False)
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        "source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+    })
+    return driver
