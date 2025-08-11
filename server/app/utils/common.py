67445eef9c9bcdeeca4b615a76de12839185a5c2
Shanna Noe <snoe.prsvr@gmail.com>
2025-08-11 05:10:14 -0400

    Created file helper for scrapers

---
+++ server/app/utils/common.py
@@ -0,0 +1,42 @@
+import json
+
+TECH_KEYWORDS = [
+    # "software engineer",
+    # "front-end developer", 
+
+    "software engineer",
+    "front-end developer", 
+
+    # "back-end developer",
+    # "full-stack developer",
+    # "mobile app developer", 
+    # "web developer",
+
+    # "wordpress developer", "shopify developer",
+    "react developer",
+    "vue.js developer", "angular developer", "javascript developer",
+
+    # "wordpress developer", "shopify developer", "react developer",
+    # "vue.js developer", "angular developer", "javascript developer",
+
+    # "typescript developer", "html/css developer", "ui developer",
+    # "ux/ui developer", "web designer", "interaction designer",
+    # "accessibility specialist", "devops engineer", "qa engineer",
+    # "data analyst", "data scientist", "data engineer",
+    # "machine learning engineer", "ai developer", "python engineer",
+    # "python developer", "python web developer", "python data scientist",
+    # "python full stack developer", "cloud engineer", "cloud architect",
+    # "systems administrator", "network engineer", "site reliability engineer",
+    # "platform engineer", "product manager", "technical product manager",
+    # "ux designer", "ui designer", "cybersecurity analyst", "security engineer",
+    # "information security manager", "it support specialist", "help desk technician",
+    # "soc analyst", "blockchain developer", "ar/vr developer", "robotics engineer",
+    # "prompt engineer", "technical program manager", "database administrator",
+    # "etl developer", "solutions architect", "scrum master", "technical writer",
+    # "api integration specialist", "web performance engineer",
+    # "web accessibility engineer", "seo specialist", "web content manager"
+]
+
+LOCATION = "remote"
+PAGES_PER_KEYWORD = 2
+MAX_DAYS = 5
